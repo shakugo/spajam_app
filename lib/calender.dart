2e55310b71b04cce87853836989d172708db9245
tonchan <cleartone1216@gmail.com>
2020-10-10 18:06:36 +0900

    add page and top page

---
+++ lib/calender.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CalenderPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calender"),
+      ),
+      body: Container(
+        height: double.infinity,
+        color: Colors.red,
+      ),
+    );
+  }
+}
